ec19b939d0fe33eaa280bc8ef355c911647ee7be
victordavila2311 <vdavila@unal.edu.co>
2024-04-26 16:48:31 -0500

    cracion pagina grabaciones y añadido webcam

--- src/componentes/headerventanas.tsx
+++ src/componentes/headerventanas.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import '../App.css';
 import { propsVentanaInicio } from '../code/interfaces';
 import { formatColor } from '../code/helpers';
-import './headerventana.css';
-export function opciones(){
+import './headerventanas.css';
+export function Opciones(){
+    let w:string='33.33333vw';
     return(
         <div className='opciones'>
-            <div>Practica</div>
-            <div>Grabaciones</div>
-            <div>Datos</div>
+            <div style={{backgroundColor:formatColor("azul"), 
+            width:w, 
+            textAlign:'center', height:'5vh',
+            color:formatColor("blanco")}}>Practica</div>
+            <div style={{backgroundColor:formatColor("cafe"), 
+            width:w, 
+            textAlign:'center', 
+            color:formatColor("blanco")}}>Grabaciones</div>
+            <div style={{backgroundColor:formatColor("verde"), 
+            width:w, 
+            textAlign:'center', 
+            color:formatColor("blanco")}}>Datos</div>
         </div>
     )
 }
--- src/ventanas/Practica.tsx
+++ src/ventanas/Practica.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import React,{useRef, useEffect, useState, useCallback} from 'react';
 import '../App.css';
+import './Practica.css';
+import Webcam from "react-webcam";
 import { propsVentanaPractica } from '../code/interfaces';
+import { Opciones } from '../componentes/headerventanas';
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
+let videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: FACING_MODE_ENVIRONMENT
+};
+
+
+
+
+
+
 
 export function Practica(props: propsVentanaPractica) {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_ENVIRONMENT)
+  const handleClick = React.useCallback(() => {
+    setFacingMode(
+      prevState =>
+        prevState === FACING_MODE_USER
+          ? FACING_MODE_ENVIRONMENT
+          : FACING_MODE_USER
+    );
+  }, []);
+  
   return (
     <div className='Practica'>
 
+      <Opciones />
+      
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>start</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>end </button>
+            
+          </div>
+          <div style={{alignContent:'center'}}>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{
+                facingMode
+              }}
+            />
+          </div>
+          
+        </>
+      )}
+      
     </div>
   );
 }
